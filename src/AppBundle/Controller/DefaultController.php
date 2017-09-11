2ee3a3b65f4cb63162463d86836400c4360aeb55
Dan Woodbury <dan@woodbury.me.uk>
2017-09-11 16:49:02 +0100

    BST-110 rights policy, terms & conditions

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,34 +12,54 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request) : Response
+    public function indexAction(Request $request): Response
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
 
     /**
      * @Route("/confirm-lead-email/{slug}", name="confirm_lead_email")
      */
-    public function confirmLeadEmailAction(Request $request, string $slug) : Response
+    public function confirmLeadEmailAction(Request $request, string $slug): Response
     {
         $apiClient = new \BeautyStack\ApiClient\ApiClient();
         $apiClient->getConfig()->setHost('http://api.beautystack.local');
-        $apiInstance = new \BeautyStack\ApiClient\Api\DirectoryApi($apiClient);
+        $apiInstance       = new \BeautyStack\ApiClient\Api\DirectoryApi($apiClient);
         $updateLeadRequest = new \BeautyStack\ApiClient\Model\UpdateLeadRequest();
         $updateLeadRequest->setStatus('CONFIRMED');
         $result = $apiInstance->directoryLeadsIdPatch($slug, $updateLeadRequest);
 
-       return $this->redirectToRoute('email_confirmed', array(), 301);
+        return $this->redirectToRoute('email_confirmed', [], 301);
     }
 
     /**
      * @Route("/confirmed", name="email_confirmed")
      */
-    public function emailConfirmed() : Response
+    public function emailConfirmed(): Response
     {
         return $this->render('default/confirmed.html.twig');
+    }
+
+    /**
+     * @Route("/privacy", name="rights")
+     */
+    public function privacyAction(Request $request): Response
+    {
+        return $this->render('default/privacy.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/termsofservice", name="terms")
+     */
+    public function termsAction(Request $request): Response
+    {
+        return $this->render('default/terms.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
     }
 }
