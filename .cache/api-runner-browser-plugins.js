f862dbdd482891ff9d8082ddfb03b347411043b6
Eik Hunter <eikhunter@Eiks-MacBook-Pro.local>
2018-12-20 17:53:27 +0000

    Add new assets

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/eikhunter/Workspace/fe.homepage/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/eikhunter/Workspace/fe.homepage/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/eikhunter/Workspace/fe.homepage/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/eikhunter/Workspace/fe.homepage/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/eikhunter/Workspace/fe.homepage/src/pages/index.js")),
+  "component---src-pages-privacy-js": preferDefault(require("/Users/eikhunter/Workspace/fe.homepage/src/pages/privacy.js")),
+  "component---src-pages-terms-js": preferDefault(require("/Users/eikhunter/Workspace/fe.homepage/src/pages/terms.js"))
+}
+
