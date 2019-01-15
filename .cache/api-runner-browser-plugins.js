64e083edd450da0114da088a2a9976d93292d9f4
Eik Hunter <eikhunter@Eiks-MacBook-Pro.local>
2019-01-15 14:19:24 +0000

    Add .cache to git ignore

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/eikhunter/Workspace/fe.homepage/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/eikhunter/Workspace/fe.homepage/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
